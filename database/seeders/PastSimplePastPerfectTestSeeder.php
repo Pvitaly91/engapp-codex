6c858cdcc26eec5c671847161044d3ade709b8e1
Pvitaly91 <Pvitaly91@gmail.com>
2025-08-28 20:04:49 +0300

    Replace blanks with answer markers in past tense seeder

--- database/seeders/PastSimplePastPerfectTestSeeder.php
+++ database/seeders/PastSimplePastPerfectTestSeeder.php
@@ -32,7 +32,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
             1 => [
                 'questions' => [
                     [
-                        'q' => "I showed him a letter that I ____.",
+                        'q' => 'I showed him a letter that I {a1}.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => "'d written", 'verb_hint' => null],
                         ],
@@ -41,7 +41,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "I wrote a letter and a few days later I ____ it to him.",
+                        'q' => 'I wrote a letter and a few days later I {a1} it to him.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'showed', 'verb_hint' => null],
                         ],
@@ -50,7 +50,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "When I looked through the window, it ____ raining.",
+                        'q' => 'When I looked through the window, it {a1} raining.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'started', 'verb_hint' => null],
                         ],
@@ -59,7 +59,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "When I looked through the window, it ____.",
+                        'q' => 'When I looked through the window, it {a1}.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'had rained', 'verb_hint' => null],
                         ],
@@ -68,7 +68,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "When you called, I ____ dinner.",
+                        'q' => 'When you called, I {a1} dinner.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => "'d had", 'verb_hint' => null],
                         ],
@@ -77,7 +77,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "When you called, I ____ dinner.",
+                        'q' => 'When you called, I {a1} dinner.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'had', 'verb_hint' => null],
                         ],
@@ -86,7 +86,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "She bought a T-shirt in the sales and ____ it to me.",
+                        'q' => 'She bought a T-shirt in the sales and {a1} it to me.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'gave', 'verb_hint' => null],
                         ],
@@ -95,7 +95,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "She gave me a T-shirt that she ____ in the sales.",
+                        'q' => 'She gave me a T-shirt that she {a1} in the sales.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'had bought', 'verb_hint' => null],
                         ],
@@ -104,7 +104,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "When the teacher arrived, I ____ my composition.",
+                        'q' => 'When the teacher arrived, I {a1} my composition.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'finished', 'verb_hint' => null],
                         ],
@@ -113,7 +113,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "When the teacher arrived, I ____ my composition.",
+                        'q' => 'When the teacher arrived, I {a1} my composition.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => "'d finished", 'verb_hint' => null],
                         ],
@@ -126,7 +126,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
             2 => [
                 'questions' => [
                     [
-                        'q' => '… I ____ my dinner.',
+                        'q' => '… I {a1} my dinner.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => "hadn't finished", 'verb_hint' => null],
                         ],
@@ -135,7 +135,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => "I couldn't open the door because I ____ the keys at work.",
+                        'q' => "I couldn't open the door because I {a1} the keys at work.",
                         'answers' => [
                             ['marker' => 'a1', 'answer' => "'d forgotten", 'verb_hint' => null],
                         ],
@@ -144,7 +144,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'When Rita travelled to France, she ____ the Louvre.',
+                        'q' => 'When Rita travelled to France, she {a1} the Louvre.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'visited', 'verb_hint' => null],
                         ],
@@ -153,7 +153,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'There were no cookies. Somebody ____ them all.',
+                        'q' => 'There were no cookies. Somebody {a1} them all.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'had eaten', 'verb_hint' => null],
                         ],
@@ -162,7 +162,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'I saw some cookies and I ____ them all.',
+                        'q' => 'I saw some cookies and I {a1} them all.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'ate', 'verb_hint' => null],
                         ],
@@ -171,7 +171,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'I locked the door and then I ____ home.',
+                        'q' => 'I locked the door and then I {a1} home.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'left', 'verb_hint' => null],
                         ],
@@ -180,7 +180,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'I saw that I ____ the door.',
+                        'q' => 'I saw that I {a1} the door.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => "hadn't locked", 'verb_hint' => null],
                         ],
@@ -189,7 +189,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'The house was dirty because nobody ____ it.',
+                        'q' => 'The house was dirty because nobody {a1} it.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'had cleaned', 'verb_hint' => null],
                         ],
@@ -198,7 +198,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'She gave me back my book because she ____ reading it.',
+                        'q' => 'She gave me back my book because she {a1} reading it.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => "'d finished", 'verb_hint' => null],
                         ],
@@ -207,7 +207,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'When she finished the book, she ____ it back to me.',
+                        'q' => 'When she finished the book, she {a1} it back to me.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'gave', 'verb_hint' => null],
                         ],
@@ -220,7 +220,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
             3 => [
                 'questions' => [
                     [
-                        'q' => 'Jim (not call) yet.',
+                        'q' => 'Jim {a1} yet.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => "hadn't called", 'verb_hint' => 'not/call'],
                         ],
@@ -229,7 +229,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'I (call) Jim.',
+                        'q' => 'I {a1} Jim.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'called', 'verb_hint' => 'call'],
                         ],
@@ -238,7 +238,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'the trousers that I (buy) in Camden market.',
+                        'q' => 'the trousers that I {a1} in Camden market.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'had bought', 'verb_hint' => 'buy'],
                         ],
@@ -247,7 +247,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'I (decide) to wear a skirt.',
+                        'q' => 'I {a1} to wear a skirt.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'decided', 'verb_hint' => 'decide'],
                         ],
@@ -256,7 +256,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'they (get) furious.',
+                        'q' => 'they {a1} furious.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'got', 'verb_hint' => 'get'],
                         ],
@@ -265,7 +265,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => '(I/make) a terrible mistake?',
+                        'q' => '{a1} a terrible mistake?',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'Had I made', 'verb_hint' => 'I/make'],
                         ],
@@ -274,7 +274,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'John asked me if I (see) his dog.',
+                        'q' => 'John asked me if I {a1} his dog.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'had seen', 'verb_hint' => 'see'],
                         ],
@@ -283,7 +283,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'the dog (run) away during some fireworks.',
+                        'q' => 'the dog {a1} away during some fireworks.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'had run', 'verb_hint' => 'run'],
                         ],
@@ -292,7 +292,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'Where (I/put) the keys?',
+                        'q' => 'Where {a1} the keys?',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'had I put', 'verb_hint' => 'I/put'],
                         ],
@@ -301,7 +301,7 @@ class PastSimplePastPerfectTestSeeder extends Seeder
                         'level' => 'B1',
                     ],
                     [
-                        'q' => 'I (try) to talk to my boss.',
+                        'q' => 'I {a1} to talk to my boss.',
                         'answers' => [
                             ['marker' => 'a1', 'answer' => 'tried', 'verb_hint' => 'try'],
                         ],
