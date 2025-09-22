f29dc3f735ecfa6f80d15199bcb99df0d89f0f9f
Pvitaly91 <Pvitaly91@gmail.com>
2025-09-22 13:39:36 +0300

    Add AI seeder for past perfect comprehensive test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -100,7 +100,8 @@ class DatabaseSeeder extends Seeder
             NegativePresentPerfectHabitsTestSeeder::class,
             MixedTenseUsageAiSeeder::class,
             Ai\MixedPerfectTenseDetailedSeeder::class,
-        
+            Ai\PastPerfectComprehensiveAiSeeder::class,
+
         ]);
     }
 }
