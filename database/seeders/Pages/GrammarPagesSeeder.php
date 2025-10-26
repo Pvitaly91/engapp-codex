69a23cc9e87cf232ca0772ac1fe6d1a7196dfc52
Pvitaly91 <Pvitaly91@gmail.com>
2025-10-26 15:58:59 +0200

    Deploy from admin panel (2025-10-26T13:58:59+00:00)

--- database/seeders/Pages/GrammarPagesSeeder.php
+++ database/seeders/Pages/GrammarPagesSeeder.php
@@ -7,7 +7,15 @@ use Database\Seeders\Pages\Conditions\MixedConditionalPageSeeder;
 use Database\Seeders\Pages\Conditions\SecondConditionalPageSeeder;
 use Database\Seeders\Pages\Conditions\ThirdConditionalPageSeeder;
 use Database\Seeders\Pages\Conditions\ZeroConditionalPageSeeder;
+use Database\Seeders\Pages\ArticlesAndQuantifiersPageSeeder;
+use Database\Seeders\Pages\DemonstrativesPageSeeder;
+use Database\Seeders\Pages\HaveGotPageSeeder;
+use Database\Seeders\Pages\IrregularVerbsPageSeeder;
 use Database\Seeders\Pages\ModalVerbsPageSeeder;
+use Database\Seeders\Pages\PronounsPageSeeder;
+use Database\Seeders\Pages\QuestionsPageSeeder;
+use Database\Seeders\Pages\ThereIsThereArePageSeeder;
+use Database\Seeders\Pages\ToBeVerbPageSeeder;
 use Database\Seeders\Pages\Tenses\FutureContinuousPageSeeder;
 use Database\Seeders\Pages\Tenses\FuturePerfectContinuousPageSeeder;
 use Database\Seeders\Pages\Tenses\FuturePerfectPageSeeder;
@@ -43,6 +51,14 @@ class GrammarPagesSeeder extends Seeder
         ThirdConditionalPageSeeder::class,
         MixedConditionalPageSeeder::class,
         ModalVerbsPageSeeder::class,
+        ArticlesAndQuantifiersPageSeeder::class,
+        DemonstrativesPageSeeder::class,
+        HaveGotPageSeeder::class,
+        IrregularVerbsPageSeeder::class,
+        PronounsPageSeeder::class,
+        QuestionsPageSeeder::class,
+        ThereIsThereArePageSeeder::class,
+        ToBeVerbPageSeeder::class,
     ];
 
     public function run(): void
