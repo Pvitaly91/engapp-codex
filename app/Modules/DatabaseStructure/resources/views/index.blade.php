1643559fce15cf517d8172f9c2798e638adf53b7
Pvitaly91 <Pvitaly91@gmail.com>
2025-10-31 23:47:48 +0200

    Add mobile horizontal scroll for content management table

--- app/Modules/DatabaseStructure/resources/views/index.blade.php
+++ app/Modules/DatabaseStructure/resources/views/index.blade.php
@@ -1514,8 +1514,8 @@
 
             <template x-if="!contentManagement.viewer.loading && !contentManagement.viewer.error && contentManagement.viewer.rows.length > 0">
               <div class="space-y-4">
-                <div class="overflow-x-auto">
-                  <table class="min-w-full divide-y divide-border/60 text-[15px]">
+                <div class="-mx-4 overflow-x-auto md:mx-0">
+                  <table class="w-full min-w-max divide-y divide-border/60 text-[15px] md:min-w-full">
                     <thead class="text-left text-xs uppercase tracking-wider text-muted-foreground">
                       <tr>
                         <template x-for="column in contentManagement.viewer.columns" :key="`cm-column-${column}`">
