1eb012e5193fbc796b71283942f78efa34676330
Pvitaly91 <Pvitaly91@gmail.com>
2025-10-31 23:29:44 +0200

    Deploy from admin panel (2025-10-31T21:29:44+00:00)

---
+++ config/database-structure.php
@@ -0,0 +1,23 @@
+<?php
+
+return array (
+  'route_prefix' => 'admin/database-structure',
+  'connection' => NULL,
+  'manual_relations' => 
+  array (
+  ),
+  'content_management' => 
+  array (
+    'menu' => 
+    array (
+      0 => 
+      array (
+        'table' => 'pages',
+        'label' => 'pages',
+      ),
+    ),
+    'table_settings' => 
+    array (
+    ),
+  ),
+);
