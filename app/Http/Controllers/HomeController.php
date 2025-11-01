434e016c04fa985b134020a34bee8fe73fff07a0
Pvitaly91 <Pvitaly91@gmail.com>
2025-11-01 23:13:59 +0200

    Revamp public home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Page;
+use App\Models\PageCategory;
+use App\Models\Question;
+use App\Models\SavedGrammarTest;
+use App\Models\Tag;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $latestTests = SavedGrammarTest::query()
+            ->withCount('questionLinks')
+            ->latest()
+            ->take(3)
+            ->get();
 
-        return view('home');
+        $featuredCategories = PageCategory::query()
+            ->withCount('pages')
+            ->orderBy('title')
+            ->take(4)
+            ->get();
+
+        $recentPages = Page::query()
+            ->with('category')
+            ->whereHas('category')
+            ->latest('updated_at')
+            ->take(4)
+            ->get();
+
+        $stats = [
+            'tests' => SavedGrammarTest::count(),
+            'questions' => Question::count(),
+            'pages' => Page::count(),
+            'tags' => Tag::count(),
+        ];
+
+        return view('home', [
+            'latestTests' => $latestTests,
+            'featuredCategories' => $featuredCategories,
+            'recentPages' => $recentPages,
+            'stats' => $stats,
+        ]);
     }
 }
