0390e25769a47400af4d177beb447fac4b683ea6
Pvitaly91 <Pvitaly91@gmail.com>
2025-11-13 21:52:09 +0200

    Add category seeder previews

--- database/seeders/Pages/Articles/SomeAnyCategorySeeder.php
+++ database/seeders/Pages/Articles/SomeAnyCategorySeeder.php
@@ -2,62 +2,20 @@
 
 namespace Database\Seeders\Pages\Articles;
 
-use App\Models\PageCategory;
-use App\Models\TextBlock;
-use App\Support\Database\Seeder;
 use Database\Seeders\Pages\Articles\Concerns\SomeAnyContent;
+use Database\Seeders\Pages\Concerns\PageCategoryDescriptionSeeder;
 
-class SomeAnyCategorySeeder extends Seeder
+class SomeAnyCategorySeeder extends PageCategoryDescriptionSeeder
 {
     use SomeAnyContent;
 
-    public function run(): void
+    protected function slug(): string
     {
-        $config = $this->someAnyContent();
-        $locale = $config['locale'] ?? 'uk';
+        return 'some-any';
+    }
 
-        $category = PageCategory::updateOrCreate(
-            ['slug' => 'some-any'],
-            [
-                'title' => $config['title'],
-                'language' => $locale,
-            ]
-        );
-
-        TextBlock::query()
-            ->where('page_category_id', $category->getKey())
-            ->whereNull('page_id')
-            ->where('seeder', static::class)
-            ->delete();
-
-        if (! empty($config['subtitle_html'])) {
-            TextBlock::create([
-                'page_id' => null,
-                'page_category_id' => $category->getKey(),
-                'locale' => $locale,
-                'type' => 'subtitle',
-                'column' => 'header',
-                'heading' => null,
-                'css_class' => null,
-                'sort_order' => 0,
-                'body' => $config['subtitle_html'],
-                'seeder' => static::class,
-            ]);
-        }
-
-        foreach ($config['blocks'] ?? [] as $index => $block) {
-            TextBlock::create([
-                'page_id' => null,
-                'page_category_id' => $category->getKey(),
-                'locale' => $locale,
-                'type' => 'box',
-                'column' => $block['column'] ?? 'left',
-                'heading' => $block['heading'] ?? null,
-                'css_class' => $block['css_class'] ?? null,
-                'sort_order' => $index + 1,
-                'body' => $block['body'] ?? null,
-                'seeder' => static::class,
-            ]);
-        }
+    protected function description(): array
+    {
+        return $this->someAnyContent();
     }
 }
