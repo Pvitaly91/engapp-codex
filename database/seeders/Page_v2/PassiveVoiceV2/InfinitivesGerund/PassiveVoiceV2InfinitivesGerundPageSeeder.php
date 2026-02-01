ee8b28c626af55ef0c184b9387d8575d2f881e40
Pvitaly91 <Pvitaly91@gmail.com>
2026-02-01 22:39:06 +0200

    Deploy from admin panel (2026-02-01T22:39:06+02:00)

---
+++ database/seeders/Page_v2/PassiveVoiceV2/InfinitivesGerund/PassiveVoiceV2InfinitivesGerundPageSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders\Page_v2\PassiveVoiceV2\InfinitivesGerund;
+
+use Database\Seeders\Pages\Concerns\GrammarPageSeeder;
+
+abstract class PassiveVoiceV2InfinitivesGerundPageSeeder extends GrammarPageSeeder
+{
+    protected function category(): array
+    {
+        return [
+            'slug' => 'passive-voice-v2-infinitives-gerund',
+            'title' => 'Інфінітив та герундій у пасиві',
+            'language' => 'uk',
+        ];
+    }
+}
