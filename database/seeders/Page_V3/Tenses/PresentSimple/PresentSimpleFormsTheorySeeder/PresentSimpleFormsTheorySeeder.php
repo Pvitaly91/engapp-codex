18edb9c8cf2f4cab96d66a8ac083a45f36dc5f25
Vitaly <pvitaly91@gmail.com>
2026-04-10 14:22:14 +0300

    feat(theory): add Tenses section with Present Simple subsection

---
+++ database/seeders/Page_V3/Tenses/PresentSimple/PresentSimpleFormsTheorySeeder/PresentSimpleFormsTheorySeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders\Page_V3\Tenses\PresentSimple;
+
+use App\Support\Database\JsonPageSeeder;
+
+class PresentSimpleFormsTheorySeeder extends JsonPageSeeder
+{
+    protected function definitionPath(): string
+    {
+        return __DIR__ . '/definition.json';
+    }
+}
