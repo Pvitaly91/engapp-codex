1c6a00d7409cbf077cf5681a8b1787cfe33ae783
Vitaly <pvitaly91@gmail.com>
2026-04-11 11:38:23 +0300

    feat(theory): add Past Perfect Continuous subsection under Tenses

---
+++ database/seeders/Page_V3/Tenses/PastPerfectContinuous/PastPerfectContinuousCategorySeeder.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__ . '/PastPerfectContinuousCategorySeeder/PastPerfectContinuousCategorySeeder.php';
---
+++ database/seeders/Page_V3/Tenses/PastPerfectContinuous/PastPerfectContinuousNegativesTheorySeeder/PastPerfectContinuousNegativesTheorySeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders\Page_V3\Tenses\PastPerfectContinuous;
+
+use App\Support\Database\JsonPageSeeder;
+
+class PastPerfectContinuousNegativesTheorySeeder extends JsonPageSeeder
+{
+    protected function definitionPath(): string
+    {
+        return __DIR__ . '/definition.json';
+    }
+}
