bcf69e9f5a6b631b1aead091fbe7f07587925f34
Vitaly <pvitaly91@gmail.com>
2026-04-15 00:59:45 +0300

    Start Conditionals Page_V3 theory branch

---
+++ database/seeders/Page_V3/Conditionals/ConditionalsHadBetterTheorySeeder/ConditionalsHadBetterTheorySeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders\Page_V3\Conditionals;
+
+use App\Support\Database\JsonPageSeeder;
+
+class ConditionalsHadBetterTheorySeeder extends JsonPageSeeder
+{
+    protected function definitionPath(): string
+    {
+        return __DIR__ . '/definition.json';
+    }
+}
