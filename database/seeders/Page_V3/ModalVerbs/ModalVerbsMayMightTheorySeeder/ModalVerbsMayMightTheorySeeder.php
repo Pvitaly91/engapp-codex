26b30ca3dd4d1b3c1db4de564c689c3ec75b2001
Vitaly <pvitaly91@gmail.com>
2026-04-14 22:11:09 +0300

    Start Phase 2 theory with Modal Verbs Page_V3 branch

---
+++ database/seeders/Page_V3/ModalVerbs/ModalVerbsMayMightTheorySeeder/ModalVerbsMayMightTheorySeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders\Page_V3\ModalVerbs;
+
+use App\Support\Database\JsonPageSeeder;
+
+class ModalVerbsMayMightTheorySeeder extends JsonPageSeeder
+{
+    protected function definitionPath(): string
+    {
+        return __DIR__ . '/definition.json';
+    }
+}
---
+++ database/seeders/Page_V3/ModalVerbs/ModalVerbsNeedNeedntDontHaveToTheorySeeder.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__ . '/ModalVerbsNeedNeedntDontHaveToTheorySeeder/ModalVerbsNeedNeedntDontHaveToTheorySeeder.php';
