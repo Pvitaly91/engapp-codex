25eb3371cb396c39fa01f473e4f8572e32c8ba8f
Vitaly <pvitaly91@gmail.com>
2026-05-16 23:46:24 +0300

    Add Past Simple theory page tests

---
+++ database/seeders/V3/Tenses/PastSimple/PastSimpleFormsAllLevelsV3Seeder.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__ . '/PastSimpleFormsAllLevelsV3Seeder/PastSimpleFormsAllLevelsV3Seeder.php';
---
+++ database/seeders/V3/Tenses/PastSimple/PastSimpleQuestionsAllLevelsV3Seeder.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__ . '/PastSimpleQuestionsAllLevelsV3Seeder/PastSimpleQuestionsAllLevelsV3Seeder.php';
