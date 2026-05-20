3e9cc15d15e2a07e6e1805c0fc7e65f640f0f413
Vitaly <pvitaly91@gmail.com>
2026-05-20 14:40:03 +0300

    Add relative clauses theory page tests

---
+++ database/seeders/V3/ClausesAndLinkingWords/OmittingRelativePronounsAllLevelsV3Seeder.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__ . '/OmittingRelativePronounsAllLevelsV3Seeder/OmittingRelativePronounsAllLevelsV3Seeder.php';
---
+++ database/seeders/V3/TheoryLinks/OmittingRelativePronounsTheoryLinksSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders\V3\TheoryLinks;
+
+use App\Support\Database\JsonTheoryLinksSeederBase;
+
+class OmittingRelativePronounsTheoryLinksSeeder extends JsonTheoryLinksSeederBase
+{
+    protected function manifestPath(): string
+    {
+        return database_path('seeders/V3/TheoryLinks/data/relative-clauses-omitting-relative-pronouns-theory-links.json');
+    }
+}
