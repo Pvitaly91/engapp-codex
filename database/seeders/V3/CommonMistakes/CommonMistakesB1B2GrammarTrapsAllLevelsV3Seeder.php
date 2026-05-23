dc0fb0932a43bfc360ee42324562aaba426dce49
Vitaly <pvitaly91@gmail.com>
2026-05-23 15:42:16 +0300

    Add common mistakes part two theory page tests

---
+++ database/seeders/V3/CommonMistakes/CommonMistakesB1B2GrammarTrapsAllLevelsV3Seeder.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Database\Seeders\V3\CommonMistakes;
+
+require_once __DIR__ . '/CommonMistakesB1B2GrammarTrapsAllLevelsV3Seeder/CommonMistakesB1B2GrammarTrapsAllLevelsV3Seeder.php';
