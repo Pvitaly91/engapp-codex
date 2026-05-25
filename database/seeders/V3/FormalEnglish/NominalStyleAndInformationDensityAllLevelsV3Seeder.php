ba6061c3d112328863ae94f5ea6aa11d4495be08
Vitaly <pvitaly91@gmail.com>
2026-05-25 22:28:49 +0300

    Add formal English tail theory tests

---
+++ database/seeders/V3/FormalEnglish/NominalStyleAndInformationDensityAllLevelsV3Seeder.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Database\Seeders\V3\FormalEnglish;
+
+require_once __DIR__ . '/NominalStyleAndInformationDensityAllLevelsV3Seeder/NominalStyleAndInformationDensityAllLevelsV3Seeder.php';
